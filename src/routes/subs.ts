51fbf71643cf7fc4ccaf4a03afd45533ebd7b417
Ryan McGuiness <rcm421@lehigh.edu>
2021-01-19 15:24:11 -0500

    Created get sub route

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -4,6 +4,7 @@ import { getRepository} from 'typeorm'
 
 import User from '../entities/User'
 import Sub from '../entities/Sub'
+import Post from '../entities/Post'
 import auth from "../middleware/auth"
 import user from '../middleware/user'
 
@@ -38,9 +39,33 @@ const createSub = async (req: Request, res: Response) => {
         return res.status(500).json({error: 'Something went wrong'})
     }
 }
+const getSub = async (req:Request , res: Response) => {
+    const name = req.params.name
+
+    try {
+        const sub = await Sub.findOneOrFail({name})
+        const posts = await Post.find({
+            where:{sub},
+            order: {createdAt:'DESC'},
+            relations:['comments', 'votes'],
+        })
+
+        sub.posts = posts
+
+        if(res.locals.user){
+            sub.posts.forEach(p => p.setUserVote(res.locals.user))
+        }
+
+        return res.json(sub)
+    } catch (err) {
+        console.log(err)
+        return res.status(404).json({sub:"Sub not found"})
+    }
+}
 
 const router = Router()
 
 router.post('/', user, auth, createSub)
+router.get('/:name', user, getSub)
 
 export default router
