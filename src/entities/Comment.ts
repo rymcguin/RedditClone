69b975c32571141c54c0bd93666bcf2adb14ec35
Ryan McGuiness <rcm421@lehigh.edu>
2021-01-22 12:27:07 -0500

    Comment Post added

--- src/entities/Comment.ts
+++ src/entities/Comment.ts
@@ -1,4 +1,4 @@
-import { Exclude } from 'class-transformer'
+import { Exclude, Expose } from 'class-transformer'
 import{BeforeInsert, Column, Entity as TOEntity, Index, JoinColumn, ManyToOne, OneToMany} from 'typeorm'
 import { makeId } from '../util/helpers'
 
@@ -34,6 +34,10 @@ export default class Comment extends Entity{
     @Exclude()
     @OneToMany(()=> Vote, (vote) => vote.comment)
     votes: Vote[]
+
+    @Expose() get voteScore(): number {
+        return this.votes?.reduce((prev, curr) => prev + (curr.value || 0), 0)
+    }
 
     protected userVote: number
     setUserVote(user: User) {
